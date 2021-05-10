93896fda447ede0d5c97f88781bbdabb50adb3df
Ahnaf Abid Khan <68134075+mrpoindexter@users.noreply.github.com>
2021-05-10 07:29:29 +0600

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+void Print(int array1[], int size)
+{
+	int* ptr = array1;
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(ptr + i) << " ";
+	}
+	cout << endl;
+}
+
+int Sum(int a, int b)
+{
+	return a + b;
+}
+
+int* selfMadeFibo(int first, int size)
+{
+	int* series = new int[size];
+	series[0] = 0;
+	int sum = first;
+
+	for (int i = 1; i < size; i++)
+	{
+		if (i >= 2)
+		{
+			sum = series[i-1] + series[i - 2];
+		}
+		series[i] = sum;
+	}
+
+	return series;
+}
+
+int main()
+{
+	int limit;
+	int startingValue;
+
+	cout << "Creating a fibonacci series of your own." << endl;
+	cout << "Enter the starting value and limit respectively: " << endl;
+	cin >> startingValue >> limit;
+	cout << endl;
+
+	Print(selfMadeFibo(startingValue, limit), limit);
+
+	system("pause>0");
+	return 0;
+}
